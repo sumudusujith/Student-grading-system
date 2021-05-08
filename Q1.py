a96f38a1ab8da7029213c1910b125b55e9439cc9
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-05-08 22:34:41 +0530

    c2

--- Q1.py
+++ Q1.py
@@ -2,3 +2,14 @@
 def checkValidity(number): #def for check the validity of the all credit marks inputs
          if number % 20 == 0 and number <= 120 and number >= 0:
             flag = True
+	else:
+            print("!!!!range error!!!!\n See intructions above(^_^)")
+            flag = False
+
+            return flag#to repeat to input again when range error occurred
+
+
+print("                       !INSTRUCTIONS!")
+print("")
+print("")
+print("1:Enter your suitable credits when the programme ask it..")
