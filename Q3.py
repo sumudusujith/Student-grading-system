d1326cc6e200ae4f557e6ae99392b8d93554f8cf
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-05-11 21:49:06 +0530

    q3 1

---
+++ Q3.py
@@ -0,0 +1,9 @@
+import time
+#def for check the validity of the all credit marks inputs
+def checkValidity(number): 
+         if number % 20 == 0 and number <= 120 and number >= 0:
+            check = True
+            
+         else:
+            print("!^^^^range error^^^^!\n See intructions above(^_^)")
+            check = False
