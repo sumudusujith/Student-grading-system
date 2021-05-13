80bf12c683075771848c08ce7651c787a95a17ad
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-05-13 20:46:54 +0530

    no message

--- Q5.py
+++ Q5.py
@@ -6,3 +6,10 @@ countB=0#count for Progress – module trailer
 countC=0#count for Do not progress – module retriever
 countD=0#count for excluded 
 for appendix in range(len(results)):#loop for 10 outcomes
+        
+	print('!!!Student-Progress outcome!!!')
+	time.sleep(3)
+	print('pass_Credit : ',results[appendix][0])#indexing for pass_credit
+	print('defer_Credit: ',results[appendix][1])#indexing for defer_credit
+	print('fail_Credit : ',results[appendix][2])#indexing for fail_credit
+	print("")
