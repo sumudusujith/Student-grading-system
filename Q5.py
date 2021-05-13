de507c4eda8454b0fbf3b34f5674d45f08fec12d
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-05-13 20:46:32 +0530

    no message

--- Q5.py
+++ Q5.py
@@ -2,3 +2,7 @@
 import time#import time for print time late
 results=[[120,0,0],[100,20,0],[100,0,20],[80,20,20],[60,40,20],[40,40,40],[20,40,60],[20,20,80],[20,0,100],[0,0,120]]#main list
 countA=0#count for progress
+countB=0#count for Progress – module trailer
+countC=0#count for Do not progress – module retriever
+countD=0#count for excluded 
+for appendix in range(len(results)):#loop for 10 outcomes
