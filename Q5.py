0123069224fafabf1687d62135a2768cf8170d70
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-05-13 20:45:53 +0530

    q4 1st

---
+++ Q5.py
@@ -0,0 +1,4 @@
+
+import time#import time for print time late
+results=[[120,0,0],[100,20,0],[100,0,20],[80,20,20],[60,40,20],[40,40,40],[20,40,60],[20,20,80],[20,0,100],[0,0,120]]#main list
+countA=0#count for progress
